636316df89e4cc5e06e71ef3b71a39b0c1b02155
Anmol Wadhwani <anmolsw@gmail.com>
2017-09-12 20:52:30 +0800

    Fix missing crystal abundance attribute

--- src/MineralCard.java
+++ src/MineralCard.java
@@ -3,13 +3,20 @@ public class MineralCard extends Card {
     private double gravity;
     private String cleavage;
     private String ecoValue;
+    private String crystalAbundance;
 
-    public MineralCard(String name, double hardness, double gravity, String cleavage, String ecoValue) {
+
+    public MineralCard(String name, double hardness, double gravity, String cleavage, String crystalAbundance, String ecoValue) {
         super(name);
         this.hardness = hardness;
         this.gravity = gravity;
         this.cleavage = cleavage;
+        this.crystalAbundance = crystalAbundance;
         this.ecoValue = ecoValue;
+    }
+
+    public String getCrystalAbundance() {
+        return crystalAbundance;
     }
 
     public double getHardness() {
