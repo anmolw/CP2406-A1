39f3bb557d1688939b2ae5bd85c5b09d33fe67ae
Anmol Wadhwani <anmolsw@gmail.com>
2017-09-13 14:05:38 +0800

    Add Supertrump instructions

--- src/SuperTrumpCard.java
+++ src/SuperTrumpCard.java
@@ -1,12 +1,21 @@
 public class SuperTrumpCard extends Card {
     private int trumpType;
+    String instructions;
 
-    public SuperTrumpCard(String name, int type) {
+
+    public SuperTrumpCard(String name, int type, String instructions) {
         super(name);
         this.trumpType = type;
+        this.instructions = instructions;
+
     }
 
     public int getTrumpType() {
         return trumpType;
+    }
+
+
+    public String getInstructions() {
+        return instructions;
     }
 }
